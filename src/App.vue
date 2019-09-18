14342cce293dac8521256029f1659e63227a9e5d
Wolf Pauw <wp@buzzrank.de>
2019-09-18 17:37:27 +0200

    Delete button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <button @click="createNewValueUnit">Add ValueUnit</button>
     <ul>
       <li v-for="valueUnit in valueUnits" :key="valueUnit.id">
-        {{valueUnit.name}} - {{valueUnit.content}}
+        {{valueUnit.name}} - {{valueUnit.content}} <button @click="deleteExistingValueUnit(valueUnit)">Delete</button>
       </li>
     </ul>
   </div>
@@ -11,8 +11,8 @@
 
 <script>
   import API, {  graphqlOperation } from '@aws-amplify/api';
-  import { createValueUnit } from "./graphql/mutations";
-  import { onCreateValueUnit } from './graphql/subscriptions';
+  import {createValueUnit, deleteValueUnit} from "./graphql/mutations";
+  import { onCreateValueUnit, onDeleteValueUnit } from './graphql/subscriptions';
   import { listValueUnits } from './graphql/queries';
 
   export default {
@@ -23,19 +23,28 @@
       }
     },
     methods :{
+      async deleteExistingValueUnit(valueUnit){
+        await API.graphql(graphqlOperation(deleteValueUnit, { input: { id: valueUnit.id } }))
+      },
       async createNewValueUnit(){
         const valueUnit = { name: "Use AppSync" , content: "Realtime and Offline" };
         await API.graphql(graphqlOperation(createValueUnit, { input: valueUnit }))
       },
       async getData(){
         const valueUnitData = await API.graphql(graphqlOperation(listValueUnits));
-        this.valueUnits.push(...this.valueUnits, ...valueUnitData.data.listValueUnits.items);
+        this.valueUnits.push( ...this.valueUnits, ...valueUnitData.data.listValueUnits.items );
       },
       subscribe(){
         API.graphql(graphqlOperation(onCreateValueUnit)).subscribe({
           next: (eventData) => {
             const valueUnit = eventData.value.data.onCreateValueUnit;
             this.valueUnits.push(valueUnit);
+          }
+        })
+        API.graphql(graphqlOperation(onDeleteValueUnit)).subscribe({
+          next: (eventData) => {
+            const valueUnit = eventData.value.data.onDeleteValueUnit;
+            this.valueUnits = this.valueUnits.filter(function(el) { return el.id !== valueUnit.id; });
           }
         })
       }
