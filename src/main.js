3de3da10157225651dd4b80bcc953f89f4194509
Raphael Kirchner <rk@customwebcode.com>
2019-09-19 10:39:20 +0200

    reformat

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import API from '@aws-amplify/api';
 import PubSub from '@aws-amplify/pubsub';
 import awsconfig from './aws-exports';
 import vuetify from './plugins/vuetify';
+
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  render: h => h(App)
+	vuetify,
+	render: h => h(App)
 }).$mount('#app');
 
