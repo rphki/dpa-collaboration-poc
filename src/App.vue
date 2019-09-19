a82e3c2aeb184828e33d15948fcb8c25b0fd1852
Raphael Kirchner <rk@customwebcode.com>
2019-09-19 10:36:50 +0200

    moved functional code into component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 	<v-app>
 		<v-app-bar app color="rgb(174, 210, 153)">
 			<v-toolbar-title class="headline text-uppercase">
-				<img class="logo" src="https://www.dpa.com/typo3conf/ext/dpa/Resources/Public/assets/images/logo.svg" alt="dpa">
+				<img class="logo" src="https://www.dpa.com/typo3conf/ext/dpa/Resources/Public/assets/images/logo.svg"
+					 alt="dpa">
 			</v-toolbar-title>
 			<v-spacer></v-spacer>
 			<v-toolbar-title class="headline">
@@ -14,64 +15,23 @@
 				<span class="font-weight-light">GraphQL &middot; DynamoDB &middot; Vue.js</span>
 			</v-toolbar-title>
 		</v-app-bar>
+
 		<v-content>
-
-			<button @click="createNewValueUnit">Add ValueUnit</button>
-			<ul>
-				<li v-for="valueUnit in valueUnits" :key="valueUnit.id">
-					{{valueUnit.name}} - {{valueUnit.content}} <button @click="deleteExistingValueUnit(valueUnit)">Delete</button>
-				</li>
-			</ul>
-
+			<ValueUnitList/>
 		</v-content>
+
 	</v-app>
 </template>
 
 <script>
-  import API, {  graphqlOperation } from '@aws-amplify/api';
-  import {createValueUnit, deleteValueUnit} from "./graphql/mutations";
-  import { onCreateValueUnit, onDeleteValueUnit } from './graphql/subscriptions';
-  import { listValueUnits } from './graphql/queries';
+	import ValueUnitList from './components/ValueUnitList';
 
-  export default {
-    name: 'app',
-    data(){
-      return {
-        valueUnits: []
-      }
-    },
-    methods :{
-      async deleteExistingValueUnit(valueUnit){
-        await API.graphql(graphqlOperation(deleteValueUnit, { input: { id: valueUnit.id } }))
-      },
-      async createNewValueUnit(){
-        const valueUnit = { name: "Use AppSync" , content: "Realtime and Offline" };
-        await API.graphql(graphqlOperation(createValueUnit, { input: valueUnit }))
-      },
-      async getData(){
-        const valueUnitData = await API.graphql(graphqlOperation(listValueUnits));
-        this.valueUnits.push( ...this.valueUnits, ...valueUnitData.data.listValueUnits.items );
-      },
-      subscribe(){
-        API.graphql(graphqlOperation(onCreateValueUnit)).subscribe({
-          next: (eventData) => {
-            const valueUnit = eventData.value.data.onCreateValueUnit;
-            this.valueUnits.push(valueUnit);
-          }
-        })
-        API.graphql(graphqlOperation(onDeleteValueUnit)).subscribe({
-          next: (eventData) => {
-            const valueUnit = eventData.value.data.onDeleteValueUnit;
-            this.valueUnits = this.valueUnits.filter(function(el) { return el.id !== valueUnit.id; });
-          }
-        })
-      }
-    },
-    created(){
-      this.getData();
-      this.subscribe();
-    }
-  };
+	export default {
+		name: 'app',
+		components: {
+			ValueUnitList,
+		}
+	};
 </script>
 
 
--- src/components/ValueUnitList.vue
+++ src/components/ValueUnitList.vue
@@ -1,11 +1,65 @@
 <template>
-  <v-container>
-    <v-layout text-center wrap>
+	<v-container>
+		<v-layout text-center wrap>
 
-    </v-layout>
-  </v-container>
+			<button @click="createNewValueUnit">Add ValueUnit</button>
+			<ul>
+				<li v-for="valueUnit in valueUnits" :key="valueUnit.id">
+					{{valueUnit.name}} - {{valueUnit.content}}
+					<button @click="deleteExistingValueUnit(valueUnit)">Delete</button>
+				</li>
+			</ul>
+
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
+	import API, {graphqlOperation} from '@aws-amplify/api';
+	import {createValueUnit, deleteValueUnit} from "../graphql/mutations";
+	import {onCreateValueUnit, onDeleteValueUnit} from '../graphql/subscriptions';
+	import {listValueUnits} from '../graphql/queries';
+
+	export default {
+		name: 'ValueUnitList',
+		data() {
+			return {
+				valueUnits: []
+			}
+		},
+		methods: {
+			async deleteExistingValueUnit(valueUnit) {
+				await API.graphql(graphqlOperation(deleteValueUnit, {input: {id: valueUnit.id}}))
+			},
+			async createNewValueUnit() {
+				const valueUnit = {name: "Use AppSync", content: "Realtime and Offline"};
+				await API.graphql(graphqlOperation(createValueUnit, {input: valueUnit}))
+			},
+			async getData() {
+				const valueUnitData = await API.graphql(graphqlOperation(listValueUnits));
+				this.valueUnits.push(...this.valueUnits, ...valueUnitData.data.listValueUnits.items);
+			},
+			subscribe() {
+				API.graphql(graphqlOperation(onCreateValueUnit)).subscribe({
+					next: (eventData) => {
+						const valueUnit = eventData.value.data.onCreateValueUnit;
+						this.valueUnits.push(valueUnit);
+					}
+				})
+				API.graphql(graphqlOperation(onDeleteValueUnit)).subscribe({
+					next: (eventData) => {
+						const valueUnit = eventData.value.data.onDeleteValueUnit;
+						this.valueUnits = this.valueUnits.filter(function (el) {
+							return el.id !== valueUnit.id;
+						});
+					}
+				})
+			}
+		},
+		created() {
+			this.getData();
+			this.subscribe();
+		}
+	};
 
 </script>
